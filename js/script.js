9c9042f37dd141233a38740b0c73bb221b01ee47
Intend-xie <844152695@qq.com>
2020-08-20 22:03:21 +0800

    Site updated: 2020-08-20 22:03:21

--- js/script.js
+++ js/script.js
@@ -34,12 +34,12 @@ layui.use(['jquery', 'element', 'carousel'], function(){
 //夜晚模式
 function switchDarkMode() {
 	if ($('body').hasClass('dark')) {
-		$("#dark").html("/img/rijianmoshi.svg");
+		$("#dark").html("🌞");
 		document.body.classList.remove('dark');
 		localStorage.setItem('noDark', '1');
 		localStorage.setItem('dark', '0');
 	} else {
-		$("#dark").html("/img/yejianmoshi.svg"); 
+		$("#dark").html("🌙"); 
 		document.body.classList.add('dark');
 		localStorage.setItem('dark', '1');
 		localStorage.setItem('noDark', '0');
